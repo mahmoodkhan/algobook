4af7fbd0a2008d3348dbaa1e2d3202a514cec16d
Mahmood Khan <mkhan1484@gmail.com>
2025-04-26 22:38:37 -0700

    Added a second method via transpose and reverse to rotate matrix

--- arrays/1.7_rotate_matrix.py
+++ arrays/1.7_rotate_matrix.py
@@ -10,7 +10,9 @@ class Solution:
         self.matrix = matrix
         self.size = len(self.matrix)
 
-    def print_matrix(self):
+    def print_matrix(self, message: str = None):
+        if message is not None:
+            print(message)
         for row in range(self.size):
             for col in range(self.size):
                 print(f'{self.matrix[row][col]:02d}', end=' ')
@@ -27,24 +29,14 @@ class Solution:
 
     def rotate_matrix_clockwise(self):
         """
-
-        The first layer is the outer layer consisting of:
-        1   2   3   4   5
-        6               10
-        11              15
-        16              20
-        21  22  23  24  25
-
-        For the first layer shown above:
-        . layer = first = 0
-        . last = 4 because 5 - 1 - 0 = 4
-        . offset = 0 - 0 = 0
+        Performing a 90 degrees clockwise rotation
         01  02  03  04  05             21 16 11 06 01 
         06  07  08  09  10             22 17 12 07 02 
         11  12  13  14  15      =>     23 18 13 08 03 
         16  17  18  19  20             24 19 14 09 04 
         21  22  23  24  25             25 20 15 10 05 
 
+        # below is a table of how values change during iterations
         layers = 2
         layer | first | last | i | offset
         0     | 0     | 4    | 0 | 0
@@ -54,9 +46,13 @@ class Solution:
         1     | 1     | 3    | 1 | 0
         1     | 1     | 3    | 2 | 1
         """
+        if self.size != len(self.matrix[0]):
+            return False
+
         layers = self.size // 2
         for layer in range(layers):
-            for i in range(layer, self.size - 1 - layer):
+            last = self.size - 1 - layer
+            for i in range(layer, last):
                 top = self.matrix[layer][i]
                 # top <- left
                 left = self.matrix[self.size - 1 - i][layer]
@@ -72,29 +68,41 @@ class Solution:
 
                 # right <- top
                 self.matrix[i][self.size - 1 - layer] = top
+        return True
 
+    def transpose(self):
+        if self.size != len(self.matrix[0]):
+            return False
 
-        # layers = self.size // 2
-        # for layer in range(layers):
-        #     first = layer
-        #     last = self.size - 1 - layer
-        #     for i in range(first, last):
-        #         offset = i - first
-        #         # print(f'{layer} | {first} | {last} | {i} | {offset}')
-        #         top = self.matrix[first][i]
-        #         # top <- left
-        #         self.matrix[first][i] = self.matrix[last-offset][first]
+        self.print_matrix("matrix before transpose:")
+        for r in range(self.size):
+            for c in range(r, self.size):
+                temp = self.matrix[r][c]
+                self.matrix[r][c] = self.matrix[c][r]
+                self.matrix[c][r] = temp
+        self.print_matrix("matrix after transpose:")
 
-        #         # left <- bottom
-        #         self.matrix[last-offset][first] = self.matrix[last][last-offset]
+    def reverse_all_rows(self):
+        for r in range(self.size):
+            for c in range(self.size//2):
+                end_row = self.size - 1 - c
+                temp = self.matrix[r][c]
+                self.matrix[r][c] = self.matrix[r][end_row]
+                self.matrix[r][end_row] = temp
 
-        #         # bottom <- right
-        #         self.matrix[last][last - offset] = self.matrix[i][last]
+    def rotate_matrix_by_transpose_and_reverse(self):
+        """
+        this is another way to rotate a matrix 90 degrees clockwise
+        """
+        self.transpose()
+        self.reverse_all_rows()
+        self.print_matrix("matrix after reversing all rows:")
 
-        #         # right <- top
-        #         self.matrix[i][last] = top
-
-
+    def rotate_matrix_example(self):
+        self.print_matrix("original matrix:")
+        # self.rotate_matrix_counter_clockwise()
+        self.rotate_matrix_clockwise()
+        self.print_matrix("matrix after rotating 90 degrees clockwise:")
 
 if __name__ == '__main__':
     matrix_90_l = [
@@ -117,8 +125,5 @@ if __name__ == '__main__':
         [21, 22, 23, 24, 25],
     ]
     s = Solution(matrix)
-    s.print_matrix()
-    # s.rotate_matrix_counter_clockwise()
-    s.rotate_matrix_clockwise()
-    print('\n')
-    s.print_matrix()
+    s.rotate_matrix_example()
+    s.rotate_matrix_by_transpose_and_reverse()
